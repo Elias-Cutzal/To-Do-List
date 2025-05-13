09dd3c80835681fb543b36113577c3b86e71ef7f
Elias-Cutzal <cuxilcutzal@gmail.com>
2025-05-13 16:15:42 -0600

    Agregando mejoras en dinamismo

--- To-Do.js
+++ To-Do.js
@@ -1,9 +1,23 @@
 let boton = document.querySelector('#botonAgregar')
 let lista = document.querySelector('.lista')
 let input = document.querySelector('#tarea')
-let body = document.querySelector('body')
+let agregar = document.querySelector('#agregar')
+let filtro = document.querySelector('#filtro')
+let inputBuscador = document.querySelector('#buscador')
+let tareas = []
+let Otrofiltro = []
+let reserva;
+let busqueda;
 
 const agregarTarea = () => {
+    input.value=input.value.trim()
+    let nuevaTarea = {
+        texto: input.value,
+        isCompleted: false,
+        html: ''
+    }
+    console.log(nuevaTarea.key)
+
     if(input.value.length == 0)
     {
         input.classList = 'form-control border-danger w-100 border-2'
@@ -22,23 +36,34 @@ const agregarTarea = () => {
         completado.innerText = 'Completado'
         let div = document.createElement('div')
         
-        li.innerText = input.value
+        li.innerText = nuevaTarea.texto
         li.appendChild(div)
         div.appendChild(eliminar)
         div.appendChild(completado)
         
         lista.appendChild(li)
+        nuevaTarea.html = li
+        
         input.value=''
         input.placeholder = 'Escribe una tarea para agregar a tu lista'
         
-        completado.addEventListener('click', () => {
+        completado.addEventListener('click', (event) => {
             li.className = 'w-100 d-flex flex-wrap align-items-center justify-content-between border border-2 border-success rounded p-1 my-1 text-decoration-line-through'
-            completado.remove()
+            tareas = tareas.map((item) => {
+                if(item.texto == event.target.previousElementSibling.parentElement.parentElement.firstChild.textContent) 
+                    {
+                        item.isCompleted = true
+                    }
+                    return item
+                })
+                completado.remove()
         })
 
 
-        eliminar.addEventListener('click', () => {
+        eliminar.addEventListener('click', (event) => {
             lista.removeChild(li)
+            tareas = tareas.filter(item => item.texto != event.target.parentElement.parentElement.firstChild.textContent)
+            console.log(tareas)
         })
 
         eliminar.addEventListener('mouseover', () => {
@@ -50,21 +75,55 @@ const agregarTarea = () => {
             li.classList.remove('border-danger')
             li.classList.add('border-primary')
         } )
+        tareas.push(nuevaTarea)
     }
+    console.log(tareas)
 }
 
+const filtroTareas = (value) => {
+    if(value == "Pendiente")
+    {
+        Otrofiltro = tareas.filter ((item) => item.isCompleted == false)
+    }
+    else if (value == 'Completados')
+    {
+        Otrofiltro = tareas.filter ((item) => item.isCompleted == true)
+    }
+    else 
+    {  
+        Otrofiltro = tareas
+    }
 
+    lista.innerHTML = ''
+    Otrofiltro.forEach((a) => {
+        lista.appendChild(a.html)
+    })
+}
 
-
+const buscador = (valor) => {
+    busqueda = tareas.filter((b) => b.texto.toLowerCase().includes(valor.toLowerCase()))
+    lista.innerHTML = ''
+    busqueda.forEach((b) => {
+        lista.appendChild(b.html)
+    })
+}
 
 boton.addEventListener('click', () => {
     agregarTarea()
 })
-body.addEventListener('keydown', (event) => {
+agregar.addEventListener('keydown', (event) => {
     if(event.key == 'Enter')
     {
         agregarTarea()
     }
+})
+
+filtro.addEventListener('change', (event) => {
+    filtroTareas(event.target.value)
+})
+
+inputBuscador.addEventListener('keyup', (event) => {
+    buscador(event.target.value)
 })
 
 
